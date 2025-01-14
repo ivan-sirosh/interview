c91eb6987b9e6a2028a7bca16661efdc77b71d14
exolive <exolive@gmail.com>
2025-01-14 14:15:06 +0100

    w

---
+++ src/zth/datastructure/hashtables/MyHashTable.java
@@ -0,0 +1,14 @@
+package zth.datastructure.hashtables;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class MyHashTable {
+  
+
+  MyHashTable(){
+
+    new HashMap<>();
+  }
+
+}
